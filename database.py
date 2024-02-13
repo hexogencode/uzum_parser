79cd67440caac1f1106fc84a6c245bc25bf595ab
Dmitriy Dogonkin <dmitriydogonkin@mail.ru>
2024-02-13 20:21:12 +0500

    v2
    - A one-to-many database binding has been created
    - Now the browser does not open when parsing
    - The program is optimized, execution time is reduced up to network capabilities

--- database.py
+++ database.py
@@ -4,10 +4,88 @@ import sqlite3
 def create_database():
     conn = sqlite3.connect('products.db')
     cursor = conn.cursor()
-    # Создание таблицы
-    cursor.execute('''CREATE TABLE IF NOT EXISTS products (
-                            id INTEGER PRIMARY KEY,
-                            title TEXT,
-                            price INTEGER,
-                            credit_price INTEGER
-                          )''')
+
+    # Создание таблицы "Categories" с полями: id, name
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Categories (
+            id INTEGER PRIMARY KEY,
+            name TEXT
+        )
+    ''')
+
+    # Создание таблицы "Products" с полями: id, title, price, credit, category_id
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Products (
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            price INTEGER,
+            credit INTEGER,
+            link TEXT,
+            category_id INTEGER,
+            FOREIGN KEY (category_id) REFERENCES Categories(id)
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+
+def add_category(name):
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+
+    # Проверка наличия категории в базе данных
+    cursor.execute('''
+        SELECT id FROM Categories WHERE name = ?
+    ''', (name,))
+    category = cursor.fetchone()
+
+    # Если категория не существует, вставляем новую категорию
+    if not category:
+        cursor.execute('''
+            INSERT INTO Categories (name) VALUES (?)
+        ''', (name,))
+        conn.commit()
+        category_id = cursor.lastrowid
+    else:
+        category_id = category[0]
+
+    conn.close()
+    return category_id
+
+
+def check_existing_product(link):
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+
+    # Проверка наличия продукта с такой же ссылкой в базе данных
+    cursor.execute('''
+        SELECT id FROM Products WHERE link = ?
+    ''', (link,))
+    existing_product = cursor.fetchone()
+
+    conn.close()
+
+    return existing_product
+
+
+def add_product(title, price, credit, link, category_name):
+    # Проверяем, существует ли уже продукт с такой же ссылкой
+    existing_product = check_existing_product(link)
+    if existing_product:
+        print("[-] Продукт уже существует в базе данных.")
+        return
+
+    # Если продукта с такой же ссылкой нет, добавляем новый продукт
+    category_id = add_category(category_name)
+
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+
+    # Вставка данных о продукте в таблицу
+    cursor.execute('''
+        INSERT INTO Products (title, price, credit, link, category_id) VALUES (?, ?, ?, ?, ?)
+    ''', (title, price, credit, link, category_id))
+
+    conn.commit()
+    conn.close()
--- parser.py
+++ parser.py
@@ -1,31 +1,22 @@
 import time
-import sqlite3
-from database import create_database
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from database import *
 
 
-def add_product(title, price, credit_price):
-    conn = sqlite3.connect('products.db')
-    cursor = conn.cursor()
-
-    # Добавление нового товара
-    cursor.execute('''INSERT INTO products (title, price, credit_price) VALUES (?, ?, ?)''',
-                   (title, price, credit_price))
-    conn.commit()
-
-
-def parse_products():
-    time.sleep(10)
-
-    product_elements = driver.find_elements(By.CLASS_NAME, 'subtitle-item')
+def parse_products(category):
+    # Поиск элементов
+    title_elements = driver.find_elements(By.CLASS_NAME, 'subtitle-item')
     price_elements = driver.find_elements(By.CSS_SELECTOR, '.currency.product-card-price.slightly.medium')
     credit_elements = driver.find_elements(
         By.XPATH, "//div[@class='badge']//span[contains(@class, 'text') and contains(text(), 'сум/мес')]")
+    link_elements = driver.find_elements(By.CLASS_NAME, 'subtitle-item')
 
-    for product_element, price_element, credit_element in zip(product_elements, price_elements, credit_elements):
+    for product_element, price_element, credit_element, link_element in zip(title_elements, price_elements,
+                                                                            credit_elements, link_elements):
         # Название
         title = product_element.text
 
@@ -37,10 +28,15 @@ def parse_products():
         credit_text = credit_element.text
         credit = int(credit_text.replace(' ', '').replace('сум/мес', ''))
 
-        add_product(title, price, credit)
+        # Ссылка на товар
+        link = link_element.get_attribute('href')
+
+        # Добавление продукта в БД
+        add_product(title, price, credit, link, category)
 
 
-def check_available():
+# При каждой итерации цикла проверяет есть ли что парсить
+def check_availability(category):
     product_page = 1
     while True:
         # Переход на следующую страницу
@@ -49,17 +45,26 @@ def check_available():
         # Ожидание появления текста и если не находит - програма брейкается
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'subtitle-item')))
         if driver.find_elements(By.XPATH, "//div[@class='title' and text()='Мы не нашли подходящие товары']"):
-            print("Товары не найдены. Программа завершена.")
+            print("[!] Товары не найдены. Программа завершена.")
             break
         else:
-            parse_products()
-            print(f'Товары с {product_page} страницы добавлены')
-            print("Товары найдены. Продолжение работы программы.")
+            parse_products(category)
+            print(f'[+] Продукты с {product_page} страницы успешно добавлены')
             product_page += 1
 
 
 if __name__ == '__main__':
-    link = input('Введите ссылку на страницу в узуме: ')
-    driver = webdriver.Chrome()
+    link = input('Введите ссылку на страницу в Uzum: ')
+
+    # Что бы не открывался браузер, он открывается в режиме headless (код можно удалить для визуала)
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x935')
+    driver = webdriver.Chrome(options=options)
+
+    # Создать базу данных если еще не создана
     create_database()
-    check_available()
+    # Добавить категорию без
+    add_category(link)
+    # Проверять если продукты кончаться - брейк
+    check_availability(link)
